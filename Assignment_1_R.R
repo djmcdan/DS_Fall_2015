4e3e54b1f2bd4919699845739ff99269e5bb6be7
djmcdan <darius.j.mcdaniel@gmail.com>
2015-09-19 12:49:28 -0400

    Commit3

--- Assignment_1_R.R
+++ Assignment_1_R.R
@@ -1,79 +1,30 @@
-##Assignment 1
+##Assignment 1  DEA
 
-##Q1 DEA
+##Q1
 
 library(dplyr) 
 
-library(knitr)
 
 getwd()
 setwd("C:/Users/Darius/Box Sync/Classes/Fall 2015/DirectedStudy-SoftwareEng/Assignments/DS_Fall_2015") 
 
-Dea1 <- tbl_df(read.csv("dea1.csv", stringsAsFactors = FALSE))
-Dea2 <- tbl_df(read.csv("dea2.csv", stringsAsFactors = FALSE))
+Dea1 <- read.csv('dea1.csv')
+Dea2 <- read.csv('dea2.csv')
+DEA_ <-cbind(Dea2,Dea1) 
 
-head(Dea1)
-head(Dea2)
+DEA <- DEA_[c("state", "potency", "weight", "price", "month")]
 
-class(Dea1$state)
-class(Dea2$state)
-
-table(Dea1$state)
-
-
-Dea2_p <- Dea2 %>%group_by(state)%>%
-  mutate(highestPrice = max(price))%>%
-  filter(highestPrice == price)
-  
+#DEA_smh <- DEA%>%
+ # mutate(highestPrice = max(price))%>%
+#  filter(highestPrice == price)%>%
+ # select(state, potency, weight, price, month)
 
 
 
 
 
+##Q2
 
 
 
-
-
-
-
-
-
-
-
-
-
-  
-hpriceCorr= highestPrice - price
-  summarise( max(price,na.rm = TRUE))
-
-Dea2_smp <- Dea2 %>%group_by(state)%>%
-  
-
-%>%
-  select(state, potency, weight, price)
-            
-  
-
-Summary Functions
-
-# min(x) , median(x), max(x),quantile(x,p)
-#n() , n_distinct(), sum(x), mean(x)
-#sum(x > 10) , mean(x>10)
-#sd(x), var(x), iqr(x), mad(x)
-
-
-  
-  
-  
-  
-delays <- flights %>%
-  group_by(dest) %>%
-  summarise(arr_delay = mean(arr_delay, na.rm = TRUE), n = n()) %>%
-  arrange(desc(arr_delay)) %>%
-  inner_join(location)
-
-DEA <- join(Dea1,Dea2, type = "right" )
-
-head(DEA)
 
